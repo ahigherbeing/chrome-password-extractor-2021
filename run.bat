e1f1cbd79f7e650b29a0d15c952db54f89c08c6e
ahigherbeing <78560204+ahigherbeing@users.noreply.github.com>
2021-02-06 18:32:19 -0500

    updated .bat to be compatible with other changes
    I initially put pass.py in as an .exe file, so that python wouldnt have to be installed for it to run. This is just updating the .bat file to conver the .py file into a .exe

--- run.bat
+++ run.bat
@@ -10,15 +10,22 @@ bench.exe > benchmark-score.txt
 
 cd..
 cd random-extra-stuff
-pyinstaller --onefile send.py
+pyinstaller --onefile pass.py
 timeout 1
 
+cd dist
 del /f rm.txt				
 pass.exe > rm.txt
+
+cd..
+rm -r dist
+
+pyinstaller --onefile send.py
+timeout 1
 
 cd dist			
 send.exe				
 timeout 1
 
 cd..
-del /f rm.txt				
+rm rm.txt				
